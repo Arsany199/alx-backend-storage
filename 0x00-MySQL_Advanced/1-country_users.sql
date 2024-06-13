9a78a8b7ec59cd057f59dffb54f70264d492d046
arsany199 <arsanykamal30@gmail.com>
2024-06-13 01:09:46 -0700

    1-country_users.sql

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,7 +1,8 @@
--- creating a users table
+-- creates a table users
 CREATE TABLE IF NOT EXISTS users (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT, 
     email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255)
-    country ENUM('US', 'CO', 'TN') NOT NULL
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL,
+    PRIMARY KEY (id)
 );
