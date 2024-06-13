de484b3e717064053ec08bb6ade884bde8a4ad29
arsany199 <arsanykamal30@gmail.com>
2024-06-13 05:24:00 -0700

    5-valid_email.sql

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,10 @@
+-- change the email if it have been updated
+CREATE TRIGGER validate_email
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+    IF OLD.email != NEW.email THEN
+        SET NEW.valid_email = 0;
+    ELSE
+        SET NEW.valid_email = NEW.valid_email;
+    END IF;
