5aae1910080281e436e1af9eccd48dc98d43aa0d
arsany199 <arsanykamal30@gmail.com>
2024-06-13 05:58:48 -0700

    10-div.sql

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -3,11 +3,11 @@ DELIMITER $$
 CREATE FUNCTION SafeDiv(a INT, b INT)
 RETURNS FLOAT DETERMINISTIC
 BEGIN
-	IF b = 0
-	THEN
-		RETURN (0)
-	ELSE
-		RETURN (a / b)
-	END IF;
+    IF (b = 0)
+    THEN
+        RETURN (0);
+    ELSE
+        RETURN (a / b);
+    END IF;
 END $$
 DELIMITER ;
