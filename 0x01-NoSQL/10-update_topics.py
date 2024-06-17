0f0b7b6c864fd523551c16a7e910e1c18a982e3b
arsany199 <arsanykamal30@gmail.com>
2024-06-17 07:41:26 -0700

    10-update_topics.py

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""module updates som docs"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """changes all topics of a collection's document based on the name"""
+    mongo_collection.update_many(
+        {'name': name},
+        {'$set': {'topics': topics}}
+)
